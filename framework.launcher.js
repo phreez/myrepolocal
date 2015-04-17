ae6a5d6f8d086e6f774049a1a20f06ea2dc3d733
phreez <1528947784@qq.com>
2015-04-17 18:04:39 +0800

    asd

--- framework.launcher.js
+++ framework.launcher.js
@@ -1,9 +1,10 @@
 // JavaScript Document
-
+alert();
 $(window).load(function() { // makes sure the whole site is loaded
+    alert();
 	$("#status").fadeOut(); // will first fade out the loading animation
 	$("#preloader").delay(350).fadeOut("slow"); // will fade out the white DIV that covers the website.
-    loadEn();
+    
 })
 
 $(document).ready(function(){
